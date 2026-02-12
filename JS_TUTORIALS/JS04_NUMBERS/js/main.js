7a69b0dca0283cd18a34bea927441685999d5342
berkaycelik1 <cberkay959@gmail.com>
2026-02-12 21:37:25 +0300

    Numbers

---
+++ JS_TUTORIALS/JS04_NUMBERS/js/main.js
@@ -0,0 +1,53 @@
+// Numbers
+
+//NaN is an acronym for Not a Number
+
+console.log(Number(false));
+
+
+
+//const myNumber = 42;
+//const myFloat = 42.0151;
+//const myString = "42.123abc";
+
+//NUMBER METHODS 
+//The Number.isInteger() method determies whether the passed value is an integer.
+
+//The Number.parseFloat() method parses an argument and returns a floating number. If a number. If a number cannot be parsed from the argument, it returns NaN.
+
+
+//console.log(Number.parseFloat(myString));
+//console.log(myFloat.toFixed());
+
+//console.log(Number.parseFloat("4.237abc").toFixed(2).toString())
+
+
+
+
+
+//console.log(Number.isInteger(myNumber));
+//console.log(Number.isInteger(myFloat));
+//console.log(Number.isInteger(myString));
+
+
+// An integer is a whole number.
+
+//const myNumber = 42;
+
+// A number with a decimal point is a float which references the "floating point"
+
+//const myFloat = 42.0;
+//const myFloat = 42.0;
+//const myString = "42";
+
+//console.log(myNumber === myFloat);
+//console.log(myNumber);
+//console.log(myFloat);
+//console.log(myNumber === myFloat);
+//console.log(myNumber === myString);
+//console.log(myString + 3);
+//console.log(Number(myString) + 3);
+//console.log(Number(myString) === myNumber);
+//console.log(Number(true));
+//console.log(Number("Berkay"));
+//console.log(Number(false));
