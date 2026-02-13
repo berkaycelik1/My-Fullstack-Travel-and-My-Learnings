e7a77a9309e2d236f4e4bbbd36f816e74111bdb9
berkaycelik1 <cberkay959@gmail.com>
2026-02-13 19:58:33 +0300

    Math Metods

---
+++ JS_TUTORIALS/JS05_MATH-METHODS/js/main.js
@@ -0,0 +1,19 @@
+// Math Methods and Properties
+
+//console.log(Math.PI);
+//console.log(Math.trunc(Math.PI));
+//console.log(Math.floor(Math.PI));
+
+//console.log(Math.pow(5, 2));
+
+//console.log(Math.min(2, 5, 9));
+//console.log(Math.max(2, 5, 9));
+
+//console.log(Math.random());
+
+// From 1 to 10
+//console.log(Math.floor(Math.random() * 10) + 1); 
+//console.log(Math.floor(Math.random() * 10) + 1); 
+//console.log(Math.floor(Math.random() * 10) + 1); 
+//console.log(Math.floor(Math.random() * 10) + 1); 
+//console.log(Math.floor(Math.random() * 10) + 1); 
