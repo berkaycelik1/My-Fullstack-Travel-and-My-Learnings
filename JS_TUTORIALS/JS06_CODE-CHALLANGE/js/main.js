dbd0fa7c00835cac2b9de2af6a0568cebe3dffb8
berkaycelik1 <cberkay959@gmail.com>
2026-02-13 20:36:08 +0300

    Code Challange(Name)

---
+++ JS_TUTORIALS/JS06_CODE-CHALLANGE/js/main.js
@@ -0,0 +1,15 @@
+// My first Code Challange
+
+// Write code that will return a random letter from my name
+
+//console.log(typeof "Berkay");
+
+/* console.log("Dave".charAt(Math.floor(Math.random() * 4)));
+console.log("Dave".charAt(Math.floor(Math.random() * 4)));
+console.log("Dave".charAt(Math.floor(Math.random() * 4)));
+console.log("Dave".charAt(Math.floor(Math.random() * 4))); */
+
+//Lets make this word for ALL names
+const anyName = "Jonathan";
+//console.log(anyName.charAt(Math.floor(Math.random() * 4)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
