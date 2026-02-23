a313edda4658b457da76a053ba5f0d30277063cd
berkaycelik1 <cberkay959@gmail.com>
2026-02-23 20:33:04 +0300

    Web Storage API

---
+++ JS_TUTORIALS/JS23_WEB-STORAGE-API/js/main.js
@@ -0,0 +1,16 @@
+/* window.alert(window.location);
+alert(location); */
+const myArray = ["eat", "sleep", "code"];
+const myObject = {
+    name: "Berkay",
+    hobbies: ["eat", "sleep", "code"],
+    logName: function() {
+        console.log(this.name);
+    }
+};
+
+localStorage.setItem("myLocalStore", JSON.stringify(myArray));
+const storeLength = localStorage.length;
+const myLocalData = JSON.parse(localStorage.getItem("myLocalStore"));
+
+console.log(storeLength);
