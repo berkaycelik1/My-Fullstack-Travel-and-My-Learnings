198c84701110a1ae4a6d064040f9e3f7f967c058
berkaycelik1 <cberkay959@gmail.com>
2026-02-26 19:50:57 +0300

    Modules

---
+++ JS_TUTORIALS/JS24_MODULES/js/main.js
@@ -0,0 +1,17 @@
+// modules
+/* import playGuitar from "./guitars.js";
+import { shredding as shred , plucking as fingerpicking } from "./guitars.js";
+
+console.log(playGuitar());
+console.log(shred());
+console.log(fingerpicking()); */
+
+import * as Guitars from "./guitars.js";
+import User from "./user.js";
+const me = new User("email@email.com", "Berkay");
+console.log(me);
+console.log(me.greeting());
+
+console.log(Guitars.playGuitar());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
