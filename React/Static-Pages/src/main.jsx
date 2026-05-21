ee5a05d81442a6cf3102a0d9a93301d035f9b6b4
berkaycelik1 <cberkay959@gmail.com>
2026-05-21 16:47:45 +0300

    Styled navbar using Flexbox"

---
+++ React/Static-Pages/src/main.jsx
@@ -0,0 +1,52 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="react-logo.png" width="40px" alt="React logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+        
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
