6389e1bd7c97a6cacc04dd0c9a38d8bb52613128
berkaycelik1 <cberkay959@gmail.com>
2026-05-29 18:16:53 +0300

    Travel Journal - Entry Component

---
+++ React/data-driven-react/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="world-icon.png" alt="world icon" />
+            <span>My travel journal</span>
+        </header>
+    )
+}
