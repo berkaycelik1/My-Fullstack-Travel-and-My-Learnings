15269537f2f39aa198f711740d707d67ab947f81
berkaycelik1 <cberkay959@gmail.com>
2026-06-28 17:01:06 +0300

    .

--- Node.js Tutorials/app.js
+++ Node.js Tutorials/app.js
@@ -1,3 +1,6 @@
-const {readFileSync} = require('fs');
-const fs = require('fs');
-fs.read
+const {readFileSync, writeFileSync} = require('fs');
+
+const first = readFileSync('./content/first.txt', 'utf8');
+const second = readFileSync('./content/second.txt', 'utf8');
+
+console.log(first, second)
