5e994c4462015f6569ecd0b0c107bcf98d8f0022
berkaycelik1 <cberkay959@gmail.com>
2026-06-28 16:52:38 +0300

    ..

--- Node.js Tutorials/app.js
+++ Node.js Tutorials/app.js
@@ -1,12 +1,3 @@
-const path = require('path')
-
-console.log(path.sep)
-
-const filePath = path.join('/content', 'subfolder', 'text.txt')
-console.log(filePath)
-
-const base = path.basename(filePath)
-console.log(base)
-
-const absolute = path.resolve(__dirname, 'content', 'subfolder', 'text.txt');
-console.log(absolute)
+const {readFileSync} = require('fs');
+const fs = require('fs');
+fs.read
