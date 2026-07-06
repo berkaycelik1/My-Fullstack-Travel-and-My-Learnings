7f30e64213ef3600a31cee82253ee5f4f07c169c
berkaycelik1 <cberkay959@gmail.com>
2026-07-06 17:04:17 +0300

    async patterns - node's native option

--- Node.js Tutorials/01-node-tutorial/app.js
+++ Node.js Tutorials/01-node-tutorial/app.js
@@ -1,17 +1,20 @@
-const {readFile, writeFile} = require ('fs');
-const util = require('util');
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const {readFile, writeFile} = require ('fs').promises
+// const util = require('util');
+// const readFilePromise = util.promisify(readFile);
+// const writeFilePromise = util.promisify(writeFile);
 
 
 const start = async() => {
     try {
-        const first = await readFilePromise('./content/first.txt', 'utf-8');
-        const second = await readFilePromise('./content/second.txt', 'utf-8');
-        await writeFilePromise('./content/result-mind-grenade.txt', `THIS IS AWESOME : ${first} ${second}`)
+        const first = await readFile('./content/first.txt', 'utf-8');
+        const second = await readFile('./content/second.txt', 'utf-8');
+        await writeFile('./content/result-mind-grenade.txt',
+        `THIS IS AWESOME : ${first} ${second}`,
+        { flag : 'a' }
+    )
         console.log(first, second)
     } catch (error) {
-        console.log(error)
+        console.log(error) 
     } 
     
 }
