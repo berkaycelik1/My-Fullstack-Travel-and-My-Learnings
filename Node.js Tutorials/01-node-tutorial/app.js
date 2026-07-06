a6be42cf8073281c15edabb2eb3e0aa0bb82f5ea
berkaycelik1 <cberkay959@gmail.com>
2026-07-06 17:36:56 +0300

    events emitter - code example

--- Node.js Tutorials/01-node-tutorial/app.js
+++ Node.js Tutorials/01-node-tutorial/app.js
@@ -1,39 +1,9 @@
-const {readFile, writeFile} = require ('fs').promises
-// const util = require('util');
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const EventEmitter = require('events');
 
+const customEmitter = new EventEmitter()
 
-const start = async() => {
-    try {
-        const first = await readFile('./content/first.txt', 'utf-8');
-        const second = await readFile('./content/second.txt', 'utf-8');
-        await writeFile('./content/result-mind-grenade.txt',
-        `THIS IS AWESOME : ${first} ${second}`,
-        { flag : 'a' }
-    )
-        console.log(first, second)
-    } catch (error) {
-        console.log(error) 
-    } 
-    
-}
+customEmitter.on('response', ()=>{
+    console.log(`data received`)
+})
 
-start()
-
-// const getText = (path) =>{
-//     return new Promise((resolve, reject) => {
-//      readFile(path, 'utf-8', (err, data) => {
-//     if(err){
-//         reject(err)
-//     } else {
-//         resolve(data)
-//     }
-// })
-// })
-// }
-
-
-// getText('./content/first.txt')
-//  .then((result) => console.log(result))
-//   .catch((err) => console.log(err))
+customEmitter.emit('response')
