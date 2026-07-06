c0ab9628880d7f3c8ec46ee4a64589cfc6db8375
berkaycelik1 <cberkay959@gmail.com>
2026-07-06 16:05:04 +0300

    async patterns - refactor to async

--- Node.js Tutorials/01-node-tutorial/app.js
+++ Node.js Tutorials/01-node-tutorial/app.js
@@ -12,7 +12,19 @@ const getText = (path) =>{
 })
 }
 
-getText('./content/first.txt')
- .then((result) => console.log(result))
-  .catch((err) => console.log(err))
+const start = async() => {
+    try {
+        const first = await getText('./content/first.txt');
+        const second = await getText('./content/second.txt');
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    } 
+    
+}
 
+start()
+
+// getText('./content/first.txt')
+//  .then((result) => console.log(result))
+//   .catch((err) => console.log(err))
