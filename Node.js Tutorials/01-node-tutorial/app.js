50248966e9faf7af06d64beb5dca0060ed411c5a
berkaycelik1 <cberkay959@gmail.com>
2026-07-09 17:08:08 +0300

    streams - http example

--- Node.js Tutorials/01-node-tutorial/app.js
+++ Node.js Tutorials/01-node-tutorial/app.js
@@ -1,11 +1,16 @@
-const { createReadStream } = require ('fs');
+var http = require('http')
+var fs = require('fs')
 
-const stream = createReadStream('./content/big.txt', {
-    highWaterMark:90000,
+http
+  .createServer(function (req, res) {
+//     const text = fs.readFileSync('./content/big.txt', 'utf8')
+//     res.end(text)
+const fileStream = fs.createReadStream('./content/big.txt', 'utf8');
+fileStream.on('open', () =>{
+fileStream.pipe(res)
 })
-
-stream.on('data', (result) =>{
-    console.log(result)
+fileStream.on('error',(err)=>{
+ res.end(err)
 })
-
-stream.on('error', (err)=> console.log(err))
+})
+  .listen(5001)
