0a3e8ebbdb7ad1d385617931e50b4d5b39f7a7db
berkaycelik1 <cberkay959@gmail.com>
2026-07-21 21:58:17 +0300

    http-app example

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -2,7 +2,10 @@ const http = require('http')
 const {readFileSync, read} = require('fs');
 
 // get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyles = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 
 const server = http.createServer((req,res)=>{
@@ -18,6 +21,24 @@ const server = http.createServer((req,res)=>{
     else if(url === '/about'){
         res.writeHead(200, {'content-type':'text/html'})
         res.write('<h1>about page</h1>')
+        res.end()
+        }
+        // styles
+    else if(url === '/styles.css'){
+        res.writeHead(200, {'content-type':'text/css'})
+        res.write(homeStyles)
+        res.end()
+        }
+        // image/logo
+    else if(url === '/logo.svg'){
+        res.writeHead(200, {'content-type':'image/svg'})
+        res.write(homeImage)
+        res.end()
+        }
+        // logic
+    else if(url === '/browser-app.js'){
+        res.writeHead(200, {'content-type':'text/javascript'})
+        res.write(homeLogic)
         res.end()
         }
         // 404 
