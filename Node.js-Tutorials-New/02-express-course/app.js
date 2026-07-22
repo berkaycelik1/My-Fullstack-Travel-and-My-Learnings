5ddfbf56a85ea19edb026e5db7e7fe92610ee7ce
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 13:29:22 +0300

    get

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,52 +1,17 @@
-const http = require('http')
-const {readFileSync, read} = require('fs');
-
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+const express = require('express');
+const app = express()
 
 
-const server = http.createServer((req,res)=>{
-    // console.log(req.method)
-    const url = req.url;
-    // home page
-    if(url === '/'){
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write(homePage)
-        res.end()
-    }
-        // about page
-    else if(url === '/about'){
-        res.writeHead(200, {'content-type':'text/html'})
-        res.write('<h1>about page</h1>')
-        res.end()
-        }
-        // styles
-    else if(url === '/styles.css'){
-        res.writeHead(200, {'content-type':'text/css'})
-        res.write(homeStyles)
-        res.end()
-        }
-        // image/logo
-    else if(url === '/logo.svg'){
-        res.writeHead(200, {'content-type':'image/svg'})
-        res.write(homeImage)
-        res.end()
-        }
-        // logic
-    else if(url === '/browser-app.js'){
-        res.writeHead(200, {'content-type':'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-        }
-        // 404 
-    else {
-        res.writeHead(404, {'content-type':'text/html'})
-        res.write('<h1>page not found</h1>')
-        res.end()
-        }
+
+app.listen(5001, ()=>{
+    console.log('server is listening on port 5001...')
 })
 
-server.listen(5001)
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
