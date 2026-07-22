972c224e9f93a6714d31d04d20f9677c5a5d5cfa
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 15:04:39 +0300

    serve static assets using express.static middleware

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const app = express()
+const path = require('path');
+const app = express();
 
-app.get('/',(req, res)=>{
-    console.log('user hit the resource');
-    res.status(200).send('Home Page')
+// setup static and middleware
+app.use(express.static('./public'))
+
+app.get('/', (req, res)=>{
+res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
 })
 
-app.get('/about',(req,res)=>{
-     res.status(200).send('About Page')
+app.all('*', (req, res)=>{
+    res.status(404).send('resource not found')
 })
 
-app.all('*',(req, res)=>{
-    res.status(404).send('<h1>resource not found</h1>')
-})
 
 app.listen(5001, ()=>{
-    console.log('server is listening on port 5001...')
+    console.log('server is listening on port 5001....')
 })
-
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
