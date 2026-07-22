ac5d643e53a3259c9b88204338a8b30921553d82
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 13:43:34 +0300

    setup Express.js server and basic routing

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express()
 
+app.get('/',(req, res)=>{
+    console.log('user hit the resource');
+    res.send('Home Page')
+})
 
+app.get('/about',(req,res)=>{
+     res.send('About Page')
+})
 
 app.listen(5001, ()=>{
     console.log('server is listening on port 5001...')
