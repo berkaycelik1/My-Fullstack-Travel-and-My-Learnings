af405f717635867cd918f9d7b1cffa8f01261f8f
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 16:34:09 +0300

    json basics

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+const {products} = require('./data')
 
 
 app.get('/', (req, res)=>{
-    res.json([{name:'john'},{name:'susan'}])
+    res.json(products)
 })
 
 
