8cbf0720f14e878f1a2ee10f69c7562dd4c2b810
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 16:29:54 +0300

    json

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,19 +1,13 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-// setup static and middleware
-app.use(express.static('./public'))
 
-// app.get('/', (req, res)=>{
-// res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
-// })
-
-app.all('*', (req, res)=>{
-    res.status(404).send('resource not found')
+app.get('/', (req, res)=>{
+    res.json([{name:'john'},{name:'susan'}])
 })
 
 
-app.listen(5001, ()=>{
-    console.log('server is listening on port 5001....')
+app.listen(5001, () => {
+    console.log('Server is listening on port 5001....');
+    
 })
