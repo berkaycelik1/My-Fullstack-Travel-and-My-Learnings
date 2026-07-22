21ea9d1e247831aca4716430df5e622163995033
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 19:13:15 +0300

    complete query string search, limit and empty result response handling

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -47,6 +47,10 @@ app.get('/api/v1/query', (req, res)=>{
     if (limit) {
         sortedProducts = sortedProducts.slice (0, Number(limit))
     }
+    if(sortedProducts.length <1){
+        // res.status(200).send('no products mached your search');
+        return res.status(200).json({success: true, data: [] })
+    }
     res.status(200).json(sortedProducts)  
 })
 
