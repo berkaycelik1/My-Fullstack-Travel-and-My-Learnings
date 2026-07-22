160e95d451921aed4741738ac95482468a19b0db
berkaycelik1 <cberkay959@gmail.com>
2026-07-22 13:56:26 +0300

    complete Express.js basics and custom 404 route handling

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -3,11 +3,15 @@ const app = express()
 
 app.get('/',(req, res)=>{
     console.log('user hit the resource');
-    res.send('Home Page')
+    res.status(200).send('Home Page')
 })
 
 app.get('/about',(req,res)=>{
-     res.send('About Page')
+     res.status(200).send('About Page')
+})
+
+app.all('*',(req, res)=>{
+    res.status(404).send('<h1>resource not found</h1>')
 })
 
 app.listen(5001, ()=>{
