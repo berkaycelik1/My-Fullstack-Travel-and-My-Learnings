c753a78c71fd94e4bcb2605ad97155754732db89
berkaycelik1 <cberkay959@gmail.com>
2026-07-25 14:12:11 +0300

    apply logger middleware manually to multiple routes

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -1,60 +1,24 @@
-const express = require('express');
-const app = express();
-const {products} = require('./data')
+const express = require('express')
+const app = express()
+// req => middleware => res
 
+const logger = (req, res, next) =>{
+    const method = req.method;
+    const url = req.url;
+    const time = new Date().getFullYear();
+    console.log(method, url, time)
+    next()
+}
 
-app.get('/', (req, res)=>{
-    res.send('<h1> Home Page</h1><a href="/api/products">products<a>')
-})
-app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product)=>{
-        const {id, name, image} = product;
-        return {id, name, image}
-    })
-    res.json(newProducts)
-})
-
-app.get('/api/products/:productID', (req, res) => {
-    // console.log(req);
-    // console.log(req.params);
-    const {productID} = req.params;
-    const singleProduct = products.find(
-        (product)=> product.id === Number(productID) 
-    )
-    if(!singleProduct){
-        return res.status(404).send('Product Does Not Exist')
-    }
-        console.log(singleProduct)
-        return res.json(singleProduct)
-
-    })
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-  console.log(req.params)
-  res.send('hello world')
+app.get('/', logger, (req, res) => {
+    
+    res.send('Home')
 })
 
-app.get('/api/v1/query', (req, res)=>{
-    // console.log(req.query);
-    const{search, limit} = req.query
-    let sortedProducts = [...products];
-
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) =>{
-            return product.name.startsWith(search)
-        })
-    } 
-    if (limit) {
-        sortedProducts = sortedProducts.slice (0, Number(limit))
-    }
-    if(sortedProducts.length <1){
-        // res.status(200).send('no products mached your search');
-        return res.status(200).json({success: true, data: [] })
-    }
-    res.status(200).json(sortedProducts)  
+app.get('/about', logger, (req, res) => {
+    res.send('About')
 })
 
 app.listen(5001, () => {
-    console.log('Server is listening on port 5001....');
-    
+    console.log('Server is listening on port 5001....')
 })
