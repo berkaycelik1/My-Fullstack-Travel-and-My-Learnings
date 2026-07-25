4706a2a98cfaa50d7b20b888589b24ed14186989
berkaycelik1 <cberkay959@gmail.com>
2026-07-25 16:49:53 +0300

    add input validation and 400 Bad Request response to POST /login

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -11,9 +11,11 @@ app.get('/api/people', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    console.log(req.body);
-    
-    res.send('POST')
+    const { name } = req.body;
+    if(name){
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(400).send('PLease Provide Credentials')
 })
 
 app.listen(5001, () => {
