55653cc87015bee4131e9e84acad9b0d259e9411
berkaycelik1 <cberkay959@gmail.com>
2026-07-25 16:35:49 +0300

    add urlencoded middleware and basic POST endpoint for form handling

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -4,14 +4,15 @@ let { people } = require('./data')
 
 // static assets
 app.use(express.static('./methods-public'))
-
-
-
-
+// parse form data
+app.use(express.urlencoded({ extended: false}))
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success:true, data:people})
 })
 
+app.post('/login', (req, res) => {
+    res.send('POST')
+})
 
 app.listen(5001, () => {
     console.log('Server is listening on port 5001....')
