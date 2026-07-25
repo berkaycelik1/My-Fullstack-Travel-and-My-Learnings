536c9e99c88432e8bcbefa4705e4eed1bd23c0b6
berkaycelik1 <cberkay959@gmail.com>
2026-07-25 18:08:30 +0300

    add express.json middleware and POST /api/people route handling

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -6,6 +6,8 @@ let { people } = require('./data')
 app.use(express.static('./methods-public'))
 // parse form data
 app.use(express.urlencoded({ extended: false}))
+//parse json
+app.use(express.json())
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success:true, data:people})
 })
