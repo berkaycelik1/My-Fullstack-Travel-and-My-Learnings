eb68e9870bfb784f65ff963183736edaa9c5dfd2
berkaycelik1 <cberkay959@gmail.com>
2026-07-27 16:33:27 +0300

    methods -put

--- Node.js-Tutorials-New/02-express-course/app.js
+++ Node.js-Tutorials-New/02-express-course/app.js
@@ -32,6 +32,27 @@ app.post('/login', (req, res) => {
     res.status(400).send('PLease Provide Credentials')
 })
 
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    const person = people.find((person) => person.id === Number(id)) 
+
+    if(!person){
+    return res
+    .status(404)
+    .json({ success: false, msg: `No person with id ${id}`})
+    }
+    const newPeople = people.map(( person ) => {
+        if(person.id === Number(id)) {
+            person.name = name 
+        }
+        return person
+    })
+    res.status(200).json({ success: true, data: newPeople })
+})
+
+
 app.listen(5001, () => {
     console.log('Server is listening on port 5001....')
 })
